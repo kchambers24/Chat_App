8e28333bb78d096b7a816a3ce02c5509b6a843ce
Kevin Chambers <kbchambers2@gmail.com>
2016-08-21 23:14:38 -0400

    Add files via upload

--- Chat_App.js
+++ Chat_App.js
@@ -13,29 +13,32 @@ $("#login").on("submit", function(e) {
 })
 
 $(document).ready(() => {
-var getChatData = function(){
-  $.getJSON({
-      url: `${site}/messages`,
-      headers: {
-          "Authorization": "Token token=supadupasecret"
-      },
+    var getChatData = function() {
+        $.getJSON({
+            url: `${site}/messages`,
+            headers: {
+                "Authorization": "Token token=supadupasecret"
+            },
 
-      success: (response) => {
-          var chatHTML = response.data.map((post) => {
-            // var timeF = moment(`${post.created-at}`.startOf('hour').fromNow();
-             return `<li data-id=${post.id} data-username=${post.attributes.username}>${post.attributes.username}: ${post.attributes.text} <button class="delete">Delete</button></li>`;
-          })
-          $("#chatList").html(chatHTML.join("\n"));
-      }
-  });
-}
+            success: (response) => {
+                var chatHTML = response.data.map((post) => {
+                    var date = $ `{message.attributes.time}`;
+                    var formattedDate = moment(date).fromNow();
+                    return `<li data-id=${post.id}> ${post.attributes.username} : ${post.attributes.text} - ${formattedDate} <button class="delete">Delete</button></li>`;
+                    //  ${post.attributes.['created-at']}
+                    //  `<li data-id=${message.id}> ${message.attributes.username} / ${message.attributes['created-at']} </li>`);
+                })
+                $("#chatList").html(chatHTML.join("\n"));
+            }
+        });
+    }
 
-getChatData();
+    getChatData();
 
-var intervalID = function() {
-  window.setInterval(getChatData, 2000);
-};
-intervalID();
+    var intervalID = function() {
+        window.setInterval(getChatData, 2000);
+    };
+    intervalID();
 
     var date = new Date();
 
@@ -45,69 +48,52 @@ intervalID();
             //    var message = $('textarea').val();
 
             console.log("This is working");
-            // $.post({
-            //     url: `${site}/messages`,
-            //     headers: {
-            //         "Authorization": "Token token=supadupasecret"
-            //     },
-                // data: $(this).serialize(),
-                // message is the larger key
-                // data: {message: "id"},
-                // data: {username: "kchambers", text: "Hello"},
-
-                $.post({
-                    url: `${site}/messages`,
-                    headers: {
-                        "Authorization": "Token token=supadupasecret"
-                    },
-                    data: {message: {
-                        "username": "kchambers",
+            $.post({
+                url: `${site}/messages`,
+                headers: {
+                    "Authorization": "Token token=supadupasecret"
+                },
+                data: {
+                    message: {
+                        "username": "Emo Kylo Ren",
                         "text": $("#text").val()
-                    }},
-                    dataType: "json",
-                    success: function(response) {
-
-                        //when a post is successful, make a call to another GET request
-
-
-                    },
-                    error: function(error) {
-                      console.log('error!!', error);
                     }
-
-                });
-                event.preventDefault();
-                // error: function(){
-                //   alert("this isnt working");
-                // }
+                },
+                dataType: "json",
+                success: function(response) {
+                    //when a post is successful, make a call to another GET request
+                },
+                error: function(error) {
+                    console.log('error!!', error);
+                }
+            });
+            event.preventDefault();
+            // error: function(){
+            //   alert("this isnt working");
+            // }
             // }); //add inputId after <li>
-
-
-
 
             $("input").val("");
             // })
         })
         // clicking on UL and targeting the delete button on click event
-        $("#chatList").on("click", ".delete", function(event) {
-            var self = $(this);
-            console.log(self.parent().data());
-            $.ajax({
-        // calling method of Delete with .ajax and passing to the URL
-        url: `${site}/messages/${self.parent().data("id")}`,
-        headers: {
-            "Authorization": "Token token=supadupasecret"
-        },
-        type: "DELETE",
-        success: function(data) {
-            self.parent().remove();
-
-        // if a delete is a success. remove list item
-        // delete button is a child of the li
-        }
+    $("#chatList").on("click", ".delete", function(event) {
+        var self = $(this);
+        console.log(self.parent().data());
+        $.ajax({
+            // calling method of Delete with .ajax and passing to the URL
+            url: `${site}/messages/${self.parent().data("id")}`,
+            headers: {
+                "Authorization": "Token token=supadupasecret"
+            },
+            type: "DELETE",
+            success: function(data) {
+                self.parent().remove();
+                // if a delete is a success. remove list item
+                // delete button is a child of the li
+            }
         })
-        })
-
+    })
 
 })
 
